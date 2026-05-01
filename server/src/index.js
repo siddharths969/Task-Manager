23abeca10c851658441cd7fa21d50bf3478c4eb5
Siddharth <108132257+siddharths969@users.noreply.github.com>
2026-05-01 22:39:10 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,3 @@
-// server/src/index.js
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -6,9 +5,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5050;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
 app.use(cors());
 app.use(express.json());
 
@@ -23,8 +19,8 @@ app.use('/api/auth',     require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks',    require('./routes/tasks'));
 
-const server = app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 server.on('error', (err) => {
